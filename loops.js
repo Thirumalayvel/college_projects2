490761c3e6dec0a76ea0360bf7ab3107b6071d75
Thirumalayvel <thirumalayvel7@gmail.com>
2025-09-02 17:03:21 +0530

    initial push

---
+++ loops.js
@@ -0,0 +1,29 @@
+//while
+let age=0;
+
+function waitTillVoter(){
+while(age<=18){
+    console.log("age now:"+age);
+     age+=1;
+}
+console.log("successfully reached voting age");
+}
+
+//waitTillVoter();
+
+for(let i=2;i<21;i+=2){
+    console.log(i);
+}
+let value=1 ;
+do{
+    console.log("value=1");
+    value+=1;
+}while(value<=20);
+
+for(let i=21;i<=20;i++){
+    console.log("1");
+}
+
+let names =['jai','pradeep','ayyanar','venkat','vasanthakumar'];
+names[2]="Ayyanar";
+console.log(names[-1]);
